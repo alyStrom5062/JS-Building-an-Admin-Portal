6bf99e73af326b517b12dd99645e618956b7d392
alyStrom5062 <Alyenastrom00@gmail.com>
2023-04-28 21:01:53 -0700

    Assignment Attempt 1

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,45 @@
-
 // Your Code Here
+
+async function returnBooks(){
+    let response = await fetch("http://localhost:3001/listBooks", {
+        method: "GET",
+    })
+
+    let books = await response.json()
+
+    // console.log(books)
+
+    console.log(books)
+
+} // returnBooks() end
+
+returnBooks()
+
+// ==========================
+
+async function updateBook(){
+    let response = await fetch("http://localhost:3001/updateBook", {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "id": 3,
+            "title": "Legends of Arathrae",
+        }),
+    })
+
+    let updatedBook = await response.json();
+    console.log(updatedBook)
+
+}// updateBook() end
+
+updateBook()
+
+// ==================
+
+let first = 10
+let second = 3
+let third = 8
+let fourth = 4
+
